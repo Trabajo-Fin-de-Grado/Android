ac0040fcfd9e392b40bf090dbeceb6aaf1165032
Gonzalo Aguilar <zalo.agui1@gmail.com>
2019-11-07 15:43:41 +0100

    Se han añadido los proyectos Android

---
+++ MySecondApplication/app/src/test/java/com/example/mysecondapplication/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.example.mysecondapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Test
+    public void onCreate() {
+
+
+    }
+
+}
