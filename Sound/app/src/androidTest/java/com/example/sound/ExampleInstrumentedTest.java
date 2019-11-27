77d65e0ea2e46f3e1a86321506591bd49d8b0a09
Gonzalo Aguilar <zalo.agui1@gmail.com>
2019-11-27 22:43:28 +0100

    Se han implementado los tests UI Automator

--- Sound/app/src/androidTest/java/com/example/sound/ExampleInstrumentedTest.java
+++ Sound/app/src/androidTest/java/com/example/sound/ExampleInstrumentedTest.java
@@ -4,24 +4,43 @@ import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiScrollable;
+import androidx.test.uiautomator.UiSelector;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.*;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.example.sound", appContext.getPackageName());
+    }
+
+    @Test
+    public void test() throws UiObjectNotFoundException {
+
+        UiDevice mDevice = UiDevice.getInstance(getInstrumentation());
+
+        UiObject allAppsButton = mDevice.findObject(new UiSelector().description("Aplicaciones"));
+        allAppsButton.click();
+
+        UiScrollable appViews = new UiScrollable(new UiSelector().scrollable(false));
+        appViews.scrollIntoView(new UiSelector().text("Play Música"));
+
+        UiObject testingApp = mDevice.findObject(new UiSelector().text("Play Música"));
+        testingApp.clickAndWaitForNewWindow();
+
     }
 }
